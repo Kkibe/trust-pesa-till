13b87f549385481dedc680b99d1e50aa589d080b
Kibet <137826199+Kkibe@users.noreply.github.com>
2025-06-14 12:23:01 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,16 +22,10 @@ const HeroSection = () => {
                                 You can qualify for upto Ksh 100,000 instant loan to MPESA
                             </p>
                             {
-                                currentUser ? <NavLink to="/apply" className="secondary-btn text-decoration-none d-inline-block">
-                                    <span className="px-3 py-3">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-play-circle me-2" viewBox="0 0 16 16">
-                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                            <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-                                        </svg>
-                                        Apply Now
-                                    </span>
-                                </NavLink> : <NavLink to="/register" className="btn btn-md px-5 py-3 me-3 primary-btn rounded-0 border-0">
+                                currentUser ? </NavLink> : <NavLink to="/apply" className="btn btn-md px-5 py-3 me-3 primary-btn rounded-0 border-0">
                                     Get started
+                                </NavLink> : <NavLink to="/register" className="btn btn-md px-5 py-3 me-3 primary-btn rounded-0 border-0">
+                                    Apply Now
                                 </NavLink>
                             }
                         </div>
